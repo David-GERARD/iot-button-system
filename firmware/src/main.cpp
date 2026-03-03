d8c4ffa4c05551a9b8d7fb85fce6fd749ef4b83e
David-GERARD <david.gerard.23@ucl.ac.uk>
2026-03-03 13:51:55 +0000

    feat: update starter main.cpp file

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+// TODO: include any additional libraries and secrets you need here
 
 // Pin definitions
 const int buttonPin = 2;     // the number of the pushbutton pin
@@ -8,9 +9,11 @@ const int ledPin =  3;      // the number of the LED pin
 int buttonState = 0;         // variable for reading the pushbutton status
 int resetReceived = 0;       // variable for reading the reset status
 
+
 // Function prototypes
 void ledBlinkPatern(int pattern);
 void handShakeProtocol();
+
 
 // The setup function runs once when you press reset or power the board
 void setup() {
@@ -22,23 +25,28 @@ void setup() {
     pinMode(buttonPin, INPUT);
     // make sure the LED is on at the start
     digitalWrite(ledPin, HIGH); 
+
 }
 
 // The loop function runs over and over again forever
-void loop(){
-    // read the state of the pushbutton value:
+void loop() {
+
     buttonState = digitalRead(buttonPin);
-    
+
     if (buttonState == HIGH && resetReceived == 0) {
-        // if the pushbutton is pressed and the reset has been received (or we are in the first loop), turn on the LED and wait for the reset.
         Serial.println("Button pressed, waiting for reset...");
         resetReceived = 1;
         digitalWrite(ledPin, LOW);
-    } else if (resetReceived == 1){
-        // if the reset has been not been received, run the handshake protocol to wait for the reset.
-        handShakeProtocol();
     }
+
+    if (resetReceived == 1) {
+        handShakeProtocol();
+        delay(1000); // Add a delay to prevent the loop from running too fast after the handshake protocol is complete
+    }
+
+
 }
+
 
 
 void ledBlinkPatern(int pattern) {
@@ -65,7 +73,7 @@ void handShakeProtocol() {
     * When the button is pressed, the LED will turn on and stay on until the reset is received. 
     * Once the reset is received, the LED will turn off and the system will be ready for the next button press.
     * In task 1, the reset is triggered by waiting for an integer pattern to be sent through the serial monitor.
-    * In task 2, the reset is triggered by waiting for an API call to check that the device is connected to the internet.
+    * In task 2, the reset is triggered by connecting to an external server to check that the device is connected to the internet.
     * In task 3, the reset is triggered by waiting for an MQTT message that aknowledges that the device is connected to the MQTT broker.
     * In task 4, the reset is triggered by waiting for an MQTT message that sends a specific command to the device based on administrative rules defined in the cloud.
     **************************************************************/
